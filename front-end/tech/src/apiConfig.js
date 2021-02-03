d3fa0f166da348507ddf1f661daa86aaf84d31f1
mhmd <mhmdrifadah@gmail.com>
2021-02-03 14:26:50 +0300

    testing dep

--- front-end/tech/src/apiConfig.js
+++ front-end/tech/src/apiConfig.js
@@ -1,14 +1,14 @@
-const EXPRESS_PORT = 5000;
+// const EXPRESS_PORT = 5000;
 
-const apiUrls = {
-  // development: `http://localhost:${EXPRESS_PORT}`,
-  development: `/api`,
-  production: `https://YOUR_HEROKU_URL.herokuapp.com`,
-};
+// const apiUrls = {
+//   // development: `http://localhost:${EXPRESS_PORT}`,
+//   development: `/api`,
+//   production: `https://YOUR_HEROKU_URL.herokuapp.com`,
+// };
 
-const API_URL =
-  window.location.hostname === 'localhost'
-    ? apiUrls.development
-    : apiUrls.production;
+const API_URL = "/api"
+  // window.location.hostname === 'localhost'
+  //   ? apiUrls.development
+  //   : apiUrls.production;
 
 export default API_URL;
